3852cf7a221ba8670edf553183555bd45dc7d2b3
adkinsmt <adkins.matthewthomas@gmail.com>
2023-08-16 21:23:48 -0400

    Logout2

--- vite-project/src/components/Logout.jsx
+++ vite-project/src/components/Logout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logout = () => {
   const handleLogout = () => {
     localStorage.removeItem('accessToken'); 
-    React-router();
+    localStorage.removeItem('refreshToken');
   };
 
   return (
