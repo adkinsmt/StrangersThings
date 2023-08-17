902ebcbef193aaa5983e090cb6b1d8d252cc991c
tanghalleighton <tanghalleighton@yahoo.com>
2023-08-16 18:32:32 -0700

    post 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './index.css'
 import './App.css'
 
 function App() {
@@ -9,25 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div id="container">
+          <Link to="/home">Home</Link>
+          <Link to="/post">Post</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
---
+++ vite-project/src/assets/components/post.jsx
@@ -0,0 +1,8 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <h1>POST</h1>
+        </div>
+    )
+}
+
