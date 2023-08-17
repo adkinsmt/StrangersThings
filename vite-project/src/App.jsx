5cb63bb402329e46c598fcdc0944ad214b6551da
adkinsmt <adkins.matthewthomas@gmail.com>
2023-08-16 20:46:41 -0400

    LogoutCreation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from "./components/Login"
-
+import Logout from "./components/Logout"
 export default function App() {
   return(
     <>
       <Login />
+      <Logout/>
     </>
   )
 }
